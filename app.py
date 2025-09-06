a8e467ccae7acc6a077077dc3228f1727163d9b7
Sahar Kilker <sahark100@gmail.com>
2025-09-06 22:04:47 +0300

    Add initial Python application

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def greet(name):
+    """A simple greeting function"""
+    return f"Hello, {name}!"
+
+def main():
+    print(greet("Git Lab User"))
+
+if __name__ == "__main__":
+    main()
